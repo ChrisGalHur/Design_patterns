d7a22988b9c1043386385d559f094269775f4a52
ChrisGalHur <christian.galvez.hurtado@outlook.es>
2023-11-07 19:46:31 +0100

    First Commit

---
+++ src/main/java/org/example/command/Device.java
@@ -0,0 +1,12 @@
+package org.example.command;
+
+public class Device {
+
+        public void turnOn() {
+            System.out.println("Device turning on");
+        }
+
+        public void turnOff() {
+            System.out.println("Device turning off");
+        }
+}
